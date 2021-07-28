69c51cd9f51e119c1cfb97bad3d56763e04f04f7
Pavel <pl.hancharou@gmail.com>
2021-07-28 18:24:34 +0300

    feat: create webpack.config.ts in root directory

---
+++ react-components/webpack.config.ts
@@ -0,0 +1,107 @@
+import path from 'path';
+import webpack, { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+const webpackConfig = (env): Configuration => ({
+  mode: env.development ? 'development' : 'production',
+  ...(env.production || !env.development ? {} : {devtool: 'inline-source-map'}),
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsconfigPathsPlugin()]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'assets/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+                outputPath: './assets/img',
+                name: '[name].[ext]'
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              mozjpeg: {
+                progressive: true,
+                quality: 75
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+                optimizationLevel: 1
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'assets/resource',
+        use: [{
+          loader: 'file-loader',
+          options: {
+            outputPath: './assets/fonts',
+            name: '[name].[ext]'
+          }
+        }]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'index.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.PRODUCTION': env.production || !env.development,
+      'process.env.NAME': JSON.stringify(require('./package.json').name),
+      'process.env.VERSION': JSON.stringify(require('./package.json').version)
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        files: './src/**/*.{ts,tsx,js,jsx}'
+      }
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false })
+  ]
+});
+
+export default webpackConfig;
