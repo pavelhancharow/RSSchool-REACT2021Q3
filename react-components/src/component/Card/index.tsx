ce02825c911ac127ff01a536fbd4b351295de286
Pavel <pl.hancharou@gmail.com>
2021-07-30 13:22:47 +0300

    fix: create cards array

--- react-components/src/component/Card/index.tsx
+++ react-components/src/component/Card/index.tsx
@@ -1,37 +1,38 @@
 import React from 'react';
-
-import like from '../../assets/image/like.svg';
-import eye from '../../assets/image/eye.svg';
-import cart from '../../assets/image/cart.svg';
-import react from '../../assets/image/react.png';
+import cardsDB from 'src/db/indexDB';
 
 import './index.scss';
 
 const Card: React.FC = () => {
-  return (
-    <div className="cards">
-      <div className="card border-info mb-3" style={{maxWidth: '20rem'}}>
-        <img className="card-header" src={react} alt="react" />
+  const cards = cardsDB.map((card, i) => {
+    const {titleImg, likeIcon, eyeIcon, cartIcon} = card;
+    const id = i;
+
+    return (
+      <div key={id} className="card border-info mb-3" style={{maxWidth: '20rem'}}>
+        <img className="card-header" src={titleImg} alt="react" />
         <div className="card-body">
           <h4 className="card-title">Info card title</h4>
           <p className="card-text">About card</p>
         </div>
         <div className="card-footer">
           <div className="icon-container">
-            <img src={like} className="icon" alt="like" />
+            <img src={likeIcon} className="icon" alt="like" />
             <span>925</span>
           </div>
           <div className="icon-container">
-            <img src={eye} className="icon" alt="eye" />
+            <img src={eyeIcon} className="icon" alt="eye" />
             <span>10093</span>
           </div>
           <div className="icon-container">
-            <img src={cart} className="icon" alt="cart" />
+            <img src={cartIcon} className="icon" alt="cart" />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  });
+
+  return <div className="cards">{cards}</div>;
 };
 
 export default Card;
