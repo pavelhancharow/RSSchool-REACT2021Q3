d2f4c1f1d9fa2f3b60562e5a172dd55cc7a1722c
Pavel <pl.hancharou@gmail.com>
2021-08-13 16:56:36 +0300

    fix: change Searchparam component

---
+++ react-router/src/model/searchParam.tsx
@@ -0,0 +1,12 @@
+type SearchParamType = {
+  searchValue: string;
+  sort: string;
+  activePage: number;
+  showPages: number;
+  totalResults: number;
+  countPaginationOnPage: number;
+  idDetails: number;
+  qInTitle: string;
+};
+
+export default SearchParamType;
